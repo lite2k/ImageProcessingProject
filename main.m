6587ce4ff9fa3f50ef4121f2fdb8467286567ffa
Youssef El Sayed <ysayed38@gmail.com>
2021-12-16 17:24:48 +0200

    Latest updates done at uni

--- main.m
+++ main.m
@@ -1,18 +1,23 @@
-%rear = imread('C:\Users\DELL\Desktop\image processing\1 Vehicle Logo Recognition\TestCases\Case2\Case2-Rear1.jpg'); 
+rear = imread('C:\Users\DELL\Desktop\image processing\1 Vehicle Logo Recognition\TestCases\Case2\Case2-Rear1.jpg'); 
 %rear = imread('C:\Users\DELL\Desktop\image processing\1 Vehicle Logo Recognition\TestCases\Case2\Case2-Rear2.jpg');
-rear =imread('C:\Users\DELL\Desktop\image processing\1 Vehicle Logo Recognition\TestCases\Case1\Case1-Front1.bmp');
 %rear = imread('C:\Users\DELL\Desktop\image processing\1 Vehicle Logo Recognition\TestCases\Case1\Case1-Front1.bmp');
 %rear = imread('C:\Users\DELL\Desktop\image processing\1 Vehicle Logo Recognition\TestCases\Case2\Case2-Front2.jpg');
-%rear = imresize(rear ,[150 150]);
+%rear = imresize(rear ,[750 750]);
 %front = imread('');
 
 [H,W,D] = size(rear);
 gray_Rear = rgb2gray(rear);
-%gray_Rear = bwconncomp(gray_Rear);
-%figure,imshow(gray_Rear);
-bin_Rear = imbinarize(gray_Rear);
-%rear_filter=imgaussfilt(gray_Rear,1);
-%bin_Rear = imbinarize(rear_filter);
+figure,imshow(gray_Rear);
+%filtered = imgaussfilt(gray_Rear,'FilterSize',5);
+bin_Rear = im2bw(gray_Rear);
+%connected = bwconncomp(bin_Rear);
+%stat = regionprops(connected,'Centroid');
+%rectangle('position',bb,'edgecolor','r','linewidth',1.3);
+%filtered = imgaussfilt(gray_Rear,2,'FilterSize',5);
+edge = edge(bin_Rear,'canny');
+[edge1,t,r] = hough(edge);
+%bin_Rear = im2bw(edge);
+figure,imshow(edge1);
 
 %figure,imshow(bin_Rear);
 %c = round([H W]/2);
@@ -21,16 +26,16 @@ bin_Rear = imbinarize(gray_Rear);
 %cropped = imcrop(rear,rear_crop);
 %figure,imshow(cropped);
 
-rear_crop = imcrop(bin_Rear,[0 (H/3) W (H/3)]);
+rear_crop = imcrop(edge1,[0 (H/3) W (H/3)]);
 %rear_crop = bwconncomp(rear_crop);
-figure,imshow(rear_crop);
+%figure,imshow(edge1);
 orig_crop = imcrop(rear,[0 (H/3) W (H/3)]);
-newImage = orig_crop;
-newImage(:) = 0; 
+empty = rear_crop;
+empty(:) = 0; 
 
-se = strel('disk',1);
+se = strel('square',1);
 %rear_erode = imerode(rear_crop,se);
-rear_dilate = imclose(rear_crop,se);
+rear_dilate = imdilate(edge1,se);
 %figure,imshow(rear_erode);
 %figure,imshow(rear_dilate);
 
@@ -38,16 +43,50 @@ rear_dilate = imclose(rear_crop,se);
 %figure,imshow(bin_Rear);
 %plot(c(1),c(2),'*r')
 
-stats1 = regionprops (rear_dilate, 'BoundingBox');
-[L ,num] = bwlabel(rear_dilate);
-figure,imshow(rear_dilate);
+stats1 = regionprops (edge1, 'BoundingBox','Area');
+[L ,num] = bwlabel(edge1);
+figure,imshow(edge1);
 
 for R=1:num
   bb = stats1(R).BoundingBox;
-  %figure,imshow(bin_Rear);
   rectangle('position',bb,'edgecolor','r','linewidth',1.3)
 end
 [H_orig,W_orig,D_orig] = size(orig_crop);
+
+for R=1:num
+  bb = stats1(R).BoundingBox;
+  Area = stats1(R).Area;
+  X = floor(bb(1));
+  Y = floor(bb(2));
+  W =bb(3);
+  H = bb(4);
+  if(Area <= 130 )%&& Area < 480)
+      for i=Y:Y+H
+          for j=X:X+W
+              rectangle('position',bb,'edgecolor','g','linewidth',1.3)
+%               el ma3ana ll empty
+%               if(i>H+Y)
+%                    i=H+Y;
+%               if(i <= 0)
+%                    i = 1;
+%               end
+% %               
+% %               if(j>W+X)
+% %                    j = W+X;
+%               if(j <= 0)
+%                    j = 1;
+%               end
+%               %rear_crop(i,j)
+%               empty(i,j) = rear_dilate(i,j);
+          end
+      end 
+  end
+end
+new = rear_dilate - empty;
+stat2 = regionprops(new,'BoundingBox');
+[l, num] = bwlabel(new);
+%figure,imshow(empty);
+
 
 for index=1 : num
     bb = stats1(index).BoundingBox;
@@ -64,12 +103,12 @@ for index=1 : num
                     elseif(j == 0)
                         j = 1;
                     end
-                    newImage(j,i,1) = orig_crop(j,i,1);
-                    newImage(j,i,2) = orig_crop(j,i,2);
-                    newImage(j,i,3) = orig_crop(j,i,3);
+                    empty(j,i,1) = orig_crop(j,i,1);
+                    empty(j,i,2) = orig_crop(j,i,2);
+                    empty(j,i,3) = orig_crop(j,i,3);
                 end
             end
         end
 end
 
-figure,imshow(newImage);
+%figure,imshow(newImage);
